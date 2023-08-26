19aeb7934fb3bd5bf82c18206cf55c6abd615f26
Gal <galg@elad.co.il>
2023-08-26 10:25:45 +0300

    update new list style

--- src/pages/NewList.tsx
+++ src/pages/NewList.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import products from '../data/products.json';
 import { Product } from '../Types';
 import { useNavigate} from 'react-router-dom';
-
+import DefaultProduct from '../assets/product-default.png';
 
 type Props = {
     setMyList: React.Dispatch<React.SetStateAction<Product[]>>;
@@ -28,16 +28,21 @@ const NewList: React.FC<Props> = ({ setMyList }) => {
         navigate('/');
     }
   return (
-    <div>
-
-    {products && products.map((product:Product)=>(
-        <div key={product.id}>
-            <label htmlFor={`product-${product.id}`}>{product.title}</label>
-            <input type="checkbox" id={`product-${product.id}`} onChange={(e)=>handleChange(e,product)} />
+    <div className='page'>
+        <h2 className='page-title'>New List</h2>
+        <div className="new-list">
+            {products && products.map((product:Product)=>(
+                <div key={product.id} className='new-list-item'>
+                    <img src={DefaultProduct} alt={product.title}/>
+                    <label htmlFor={`product-${product.id}`}>{product.title}</label>
+                    <input type="checkbox" id={`product-${product.id}`} onChange={(e)=>handleChange(e,product)} />
+                </div>
+            ))}
         </div>
-    ))}
 
-        <button onClick={handleSave}> Save</button>
+        <div className="new-list-actions">
+            <button className='save-btn btn' onClick={handleSave}> Save</button>
+        </div>
     </div>
   )
 }
